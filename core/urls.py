ec277e759dff314b56c2dfc52c4b8a3add720cbb
AlexT1021 <alexander.torres08@inacapmail.cl>
2025-12-18 17:49:52 -0300

    Urls creados

--- core/urls.py
+++ core/urls.py
@@ -37,8 +37,15 @@ schema_view = get_schema_view(
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',),
 
-    path(''),
+    path('admin/', admin.site.urls),
+
+    #Urls de aplicaciones
+    path('ActivaFit/', include('ActivaFit.urls')),
+
+    #Urls documentacion
+    path('apidocs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redocs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
+
 ]
